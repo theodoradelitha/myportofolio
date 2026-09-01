ce0582caccd87de105759bac51f8ca48d83b9a2c
Delitha Theodora <delithatheo@gmail.com>
2026-09-01 23:59:44 +0700

    Revert "feat: create landing page with about me profile"

--- myportofolio/asgi.py
+++ myportofolio/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portofolio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myportofolio.settings')
 
 application = get_asgi_application()
--- myportofolio/wsgi.py
+++ myportofolio/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portofolio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myportofolio.settings')
 
 application = get_wsgi_application()
