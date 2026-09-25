815b04280ed1f69679210225972a002c387af5a3
Delitha Theodora <delithatheo@gmail.com>
2026-09-25 20:10:52 +0700

    feat: added HTML skeleton and views for login

--- main/views.py
+++ main/views.py
@@ -246,3 +246,16 @@ def register(request):
         "form": form,
     }
     return render(request, "components/register.html", context)
+
+def login_user(request):
+    form = AuthenticationForm(request, data=request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        login(request, form.get_user())
+        return redirect("main:show_main")
+
+    context = {
+        "name": "Delitha",
+        "form": form,
+    }
+    return render(request, "components/login.html", context)
