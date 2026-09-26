8bb3b8dd517f1129bad92c5aee58d9b901e21ac0
Delitha Theodora <delithatheo@gmail.com>
2026-09-26 15:49:35 +0700

    chore: delete the last_login Cookie on Logout

--- main/views.py
+++ main/views.py
@@ -268,4 +268,6 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    return redirect("main:show_main")
+    response = redirect("main:show_main")
+    response.delete_cookie('last_login')
+    return response
