e90740b7fcc63d9cae4400e587477cdb3110923e
Delitha Theodora <delithatheo@gmail.com>
2026-09-26 15:10:48 +0700

    updated login_user in main/views.py to add last_login cookie

--- main/views.py
+++ main/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.core import serializers
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
+import datetime
 
 def show_main(request):
     context = {
@@ -251,8 +252,11 @@ def login_user(request):
     form = AuthenticationForm(request, data=request.POST or None)
 
     if request.method == "POST" and form.is_valid():
-        login(request, form.get_user())
-        return redirect("main:show_main")
+        user = form.get_user()
+        login(request, user)
+        response = redirect("main:show_main")
+        response.set_cookie('last_login', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        return response
 
     context = {
         "name": "Delitha",
